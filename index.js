f60d0ba966ae8b974d1d210ad93be2af072d7af2
Pratham Gupta <prathamgupta2017@gmail.com>
2023-03-26 00:19:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+var cors = require('cors');
+const testresRoute = require("./route/testres");
+const userRoute = require("./route/user");
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => console.log(err));
+app.use("/testres", testresRoute);
+app.use("/users", userRoute);
+app.listen(8800, () => {
+  console.log("Running!");
+});
